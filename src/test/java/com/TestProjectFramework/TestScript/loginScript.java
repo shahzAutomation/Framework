7486755e61634712f70202e84d6657c74a573ec1
shahzAutomation <seleniumtest55187@gmail.com>
2023-04-11 01:07:50 +0530

    ScriptFileUpdated

--- src/test/java/com/TestProjectFramework/TestScript/loginScript.java
+++ src/test/java/com/TestProjectFramework/TestScript/loginScript.java
@@ -1,7 +1,6 @@
 package com.TestProjectFramework.TestScript;
 
-import org.apache.logging.log4j.*;
-
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
